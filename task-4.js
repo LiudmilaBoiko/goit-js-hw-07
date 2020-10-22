0345a02c45f82a6cd602a49c72283338ac5e44b3
Lyudmyla Boiko <itsforyou.com.ua@gmail.com>
2020-10-22 17:58:46 +0300

    re-task-3

--- task-4.js
+++ task-4.js
@@ -4,14 +4,44 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
 
-const counterValue = document.querySelector("#value");
-const decrementBtn = document.querySelector('button[data-action="decrement"]');
-const incrementBtn = document.querySelector('button[data-action="increment"]');
+// const counterValue = document.querySelector("#value");
+// const decrementBtn = document.querySelector('button[data-action="decrement"]');
+// const incrementBtn = document.querySelector('button[data-action="increment"]');
 
-const onDecrementBtnClick = () =>
-  (counterValue.textContent = Number(counterValue.textContent) - 1);
-const onIncrementBtnClick = () =>
-  (counterValue.textContent = Number(counterValue.textContent) + 1);
+// const onDecrementBtnClick = () =>
+//   (counterValue.textContent = Number(counterValue.textContent) - 1);
+// const onIncrementBtnClick = () =>
+//   (counterValue.textContent = Number(counterValue.textContent) + 1);
 
-decrementBtn.addEventListener("click", onDecrementBtnClick);
-incrementBtn.addEventListener("click", onIncrementBtnClick);
+// decrementBtn.addEventListener("click", onDecrementBtnClick);
+// incrementBtn.addEventListener("click", onIncrementBtnClick);
+
+
+
+// const decrementBtnRef = document.querySelector('[data-action="decrement"]');
+// const incrementBtnRef = document.querySelector('[data-action="increment"]');
+// const counterValueRef = document.querySelector('#value');
+
+// let currentValue = 0;
+// const increment = () => {
+//   return (counterValueRef.textContent = currentValue += 1);
+// };
+// const decrement = () => {
+//   return (counterValueRef.textContent = currentValue -= 1);
+// };
+// decrementBtnRef.addEventListener('click', decrement);
+// incrementBtnRef.addEventListener('click', increment);
+
+
+
+// var counterValue = 0;
+// function increment(){
+// 	counterValue +=1;
+//   document.getElementById('value').innerHTML = counterValue;
+// }
+// function decrement(){
+// 	counterValue -=1;
+//   document.getElementById('value').innerHTML = counterValue;
+// }
+
+
